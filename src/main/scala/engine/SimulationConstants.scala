e4ecda3d2c17ca2c766759bde8395f4a197ebe70
Marta Spadoni <marta.spadoni2@studio.unibo.it>
2021-08-28 17:04:46 +0200

    Added SimulationConstants

---
+++ src/main/scala/engine/SimulationConstants.scala
@@ -0,0 +1,19 @@
+package engine
+
+import scala.concurrent.duration.DurationInt
+
+object SimulationConstants {
+
+  val WOLF_INSTANT = 3000 millis
+
+  val FOOD_INSTANT = 6000 millis
+
+  val TEMP_INSTANT = 9000 millis
+
+  val GEN_END = 12000 millis
+
+  val MAX_BUNNIES_NUMBER = 50
+
+  val MAX_GENERATIONS_NUMBER = 1000
+
+}
