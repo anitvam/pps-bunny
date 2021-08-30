a863b84ab766356502f4f80110d6e4ee9f3f9261
Marta Spadoni <marta.spadoni2@studio.unibo.it>
2021-08-30 15:33:56 +0200

    Added first version of the SimulationEngine

---
+++ src/main/scala/engine/SimulationEngine.scala
@@ -0,0 +1,27 @@
+package engine
+
+import cats.effect.IO
+import engine.GenerationTimer.{resetTimer, waitFor}
+import engine.Simulation.{applyTemperatureDamage, bunniesEat, getBunniesNumber, getGenerationNumber, reproduction, showBunnies, startNewGeneration, wolfsEat}
+import engine.SimulationConstants.{FOOD_INSTANT, GEN_END, MAX_BUNNIES_NUMBER, MAX_GENERATIONS_NUMBER, TEMP_INSTANT, WOLF_INSTANT}
+
+object SimulationEngine {
+  def simulationLoop(): IO[Unit] = {
+    for {
+      _ <- resetTimer
+      _ <- startNewGeneration
+      _ <- showBunnies
+      _ <- waitFor(WOLF_INSTANT)
+      _ <- wolfsEat
+      _ <- waitFor(FOOD_INSTANT)
+      _ <- bunniesEat
+      _ <- waitFor(TEMP_INSTANT)
+      _ <- applyTemperatureDamage
+      _ <- waitFor(GEN_END)
+      _ <- reproduction
+      i <- getGenerationNumber
+      b <- getBunniesNumber
+      _ <- if(i < MAX_GENERATIONS_NUMBER && b < MAX_BUNNIES_NUMBER) simulationLoop() else showBunnies
+    }yield()
+  }
+}
