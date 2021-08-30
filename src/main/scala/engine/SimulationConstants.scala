5c1ee9250550132fdfb94807eabeafce4793525f
Marta Spadoni <marta.spadoni2@studio.unibo.it>
2021-08-30 15:28:57 +0200

    Added scaladoc

--- src/main/scala/engine/SimulationConstants.scala
+++ src/main/scala/engine/SimulationConstants.scala
@@ -1,19 +1,25 @@
 package engine
 
-import scala.concurrent.duration.DurationInt
+import scala.concurrent.duration.{DurationInt, FiniteDuration}
 
 object SimulationConstants {
+  /**After how many milliseconds from the start of generation the wolves can eat */
+  val WOLF_INSTANT: FiniteDuration = 3000 millis
 
-  val WOLF_INSTANT = 3000 millis
+  /**After how many milliseconds from the start of generation the bunnies can eat */
+  val FOOD_INSTANT: FiniteDuration = 6000 millis
 
-  val FOOD_INSTANT = 6000 millis
+  /**After how many milliseconds from the start of generation
+   * the bunnies are affected by high temperatures */
+  val TEMP_INSTANT: FiniteDuration = 9000 millis
 
-  val TEMP_INSTANT = 9000 millis
+  /**Duration in milliseconds of a generation*/
+  val GEN_END: FiniteDuration = 12000 millis
 
-  val GEN_END = 12000 millis
-
+  /**Maximum number of alive bunnies in the world*/
   val MAX_BUNNIES_NUMBER = 50
 
+  /**Maximum number of generations*/
   val MAX_GENERATIONS_NUMBER = 1000
 
 }
