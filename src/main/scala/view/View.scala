ae33c44b1ecaa36a3ce352e39cfddd04a71f8bd6
Martina Baiardi <martina.baiardi4@studio.unibo.it>
2021-09-01 09:57:11 +0200

    Add View Trait that expose required methods

---
+++ src/main/scala/view/View.scala
@@ -0,0 +1,7 @@
+package view
+
+/** Methods required to be implemented inside each implementation of the View */
+trait View {
+  /** Method that starts the GUI and shows it to the user */
+  def start(): Unit
+}
