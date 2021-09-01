7e1a2767f6221c34b6d142fa31321719b3247aa5
Martina Baiardi <martina.baiardi4@studio.unibo.it>
2021-09-01 09:57:49 +0200

    Add scalaFX implementation of View Trait

---
+++ src/main/scala/view/scalaFX/ScalaFXView.scala
@@ -0,0 +1,32 @@
+package view.scalaFX
+
+import controller.ScalaFXLauncher.stage
+import scalafx.application.JFXApp.PrimaryStage
+import scalafx.scene.Scene
+import scalafxml.core.{FXMLLoader, NoDependencyResolver}
+import view.scalaFX.FXControllers.BaseAppControllerInterface
+import scalafx.Includes._
+import javafx.{scene => jfxs}
+import java.io.IOException
+import view._
+
+class ScalaFXView extends View{
+  def start(): Unit = {
+    val baseAppView = getClass.getResource("/fxml/baseApp.fxml")
+    if (baseAppView == null) {
+      throw new IOException("Cannot load resource: baseApp.fxml")
+    }
+
+    val loader = new FXMLLoader(baseAppView, NoDependencyResolver)
+    loader.load()
+    val root = loader.getRoot[jfxs.Parent]
+    val rootController = loader.getController[BaseAppControllerInterface]
+    rootController.initialize()
+
+    stage = new PrimaryStage() {
+      title = "Bunnies"
+      scene = new Scene(root)
+    }
+    stage.setResizable(false)
+  }
+}
