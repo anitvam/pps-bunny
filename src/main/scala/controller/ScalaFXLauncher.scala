7c860d0b303875486969a9e3312d3b7b365ad508
Martina Baiardi <martina.baiardi4@studio.unibo.it>
2021-09-02 16:18:05 +0200

    Update Launcher to generate bunnies

--- src/main/scala/controller/ScalaFXLauncher.scala
+++ src/main/scala/controller/ScalaFXLauncher.scala
@@ -1,10 +1,13 @@
 package controller
 
+import model.Bunny.generateRandomFirstBunny
 import scalafx.application.JFXApp
 import view._
 import view.scalaFX.ScalaFXView
 
 object ScalaFXLauncher extends JFXApp{
   private val view = new ScalaFXView()
-  view.start()
+  private val someBunnies = Seq.fill(5)(generateRandomFirstBunny)
+
+  view.start(someBunnies.toSet)
 }
