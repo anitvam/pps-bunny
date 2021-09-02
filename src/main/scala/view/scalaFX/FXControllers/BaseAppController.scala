dd85adb57f30d2d332e35db583a752dca347a05c
Martina Baiardi <martina.baiardi4@studio.unibo.it>
2021-09-02 16:21:28 +0200

    Add BunnyImage conversion class and adapt BaseAppController to use a Seq[Bunny]

--- src/main/scala/view/scalaFX/FXControllers/BaseAppController.scala
+++ src/main/scala/view/scalaFX/FXControllers/BaseAppController.scala
@@ -1,5 +1,6 @@
 package view.scalaFX.FXControllers
 
+import model.Bunny
 import scalafx.animation.Timeline
 import scalafx.application.Platform
 import scalafx.scene.image.Image
@@ -8,8 +9,11 @@ import scalafx.util.Duration
 import scalafxml.core.macros.sfxml
 import view.scalaFX.components.BunnyView
 
+import java.util.Timer
+import java.util.concurrent.{Executor, ExecutorService, Executors, ScheduledExecutorService}
+
 trait BaseAppControllerInterface {
-  def initialize(): Unit
+  def initialize(bunnies: Set[Bunny]): Unit
 }
 
 @sfxml
@@ -17,22 +21,13 @@ class BaseAppController(private val simulationPane: AnchorPane,
                         private val graphPane: AnchorPane,
                         private val mutationChoicePane: AnchorPane,
                         private val factorChoicePane: AnchorPane,
-                        private val graphChoicePane: AnchorPane) extends BaseAppControllerInterface {
+                        private val graphChoicePane: AnchorPane,
+                        private var bunnyTimeline: Timeline,
+                        private var bunnyViews: Set[BunnyView] = Set.empty) extends BaseAppControllerInterface {
 
-  private val bunny = BunnyView()
 
-  val bunnyTimeline: Timeline = new Timeline {
-    onFinished = _ => {
-      keyFrames = bunny.jump()
-      this.play()
-    }
-    delay = Duration(2500)
-    autoReverse = true
-    cycleCount = 1
-    keyFrames = bunny.jump()
-  }
 
-  def initialize(): Unit = {
+  def initialize(bunnies: Set[Bunny]): Unit = {
     val hotBackground = new Image( "/environment/climate_hot.png")
     if (hotBackground == null) {
       println("An error occurred while loading resource: climate_hot.png")
@@ -46,7 +41,27 @@ class BaseAppController(private val simulationPane: AnchorPane,
       new BackgroundSize(1.0, 1.0, true, true, false, false)
     )))
 
-    simulationPane.children = bunny.imageView
+    val taken = bunnies.head
+    println(taken)
+    bunnyViews = bunnies.map(bunny => BunnyView(bunny))
+    println(bunnyViews)
+    simulationPane.children = bunnyViews.map(bunny => bunny.imageView)
+
+    val executor = Executors.newFixedThreadPool(6)
+
+    bunnyViews.foreach(bunny => {
+    val bunnyTimeline = new Timeline {
+      onFinished = _ => {
+        keyFrames = bunny.jump()
+        this.play()
+      }
+      delay = Duration(3500)
+      autoReverse = true
+      cycleCount = 1
+      keyFrames = bunny.jump()
+    }
+
     bunnyTimeline.play()
+    })
   }
 }
--- src/main/scala/view/utilities/BunnyImage.scala
+++ src/main/scala/view/utilities/BunnyImage.scala
@@ -1,63 +1,129 @@
 package view.utilities
 
+import model.Bunny
+import model.genome.{Alleles, Genes}
+import model.genome.Alleles._
 import scalafx.scene.image.Image
+import view.utilities.Conversions.{BunnyWrapper}
+import view.utilities.ImageType.{ImageType, Jumping, Normal}
 
-object BunnyImage {
-  val NORMAL_BROWN_HIGH_EARS_LONG_TEETH_NORMAL_FUR = "/bunnies/brown/high_ears/long_teeth/normal_fur/normal.png"
-  val JUMPING_BROWN_HIGH_EARS_LONG_TEETH_NORMAL_FUR = "/bunnies/brown/high_ears/long_teeth/normal_fur/jumping.png"
+object BunnyImage extends Enumeration {
 
-  val NORMAL_BROWN_HIGH_EARS_LONG_TEETH_THICK_FUR = "/bunnies/brown/high_ears/long_teeth/thick_fur/normal.png"
-  val JUMPING_BROWN_HIGH_EARS_LONG_TEETH_THICK_FUR = "/bunnies/brown/high_ears/long_teeth/thick_fur/jumping.png"
+   case class BunnyImage(urlNormal: String,
+                                urlJumping: String,
+                                alleles: List[Alleles.AlleleKind]) extends super.Val
+  import scala.language.implicitConversions
+  implicit def valueToBunnyImage(x: Value): BunnyImage = x.asInstanceOf[BunnyImage]
 
-  val NORMAL_BROWN_HIGH_EARS_NORMAL_TEETH_NORMAL_FUR = "/bunnies/brown/high_ears/normal_teeth/normal_fur/normal.png"
-  val JUMPING_BROWN_HIGH_EARS_NORMAL_TEETH_NORMAL_FUR = "/bunnies/brown/high_ears/normal_teeth/normal_fur/jumping.png"
+  val BrownHighEarsLongTeethNormalFur: BunnyImage = BunnyImage(
+                                                        "/bunnies/brown/high_ears/long_teeth/normal_fur/normal.png",
+                                                        "/bunnies/brown/high_ears/long_teeth/normal_fur/jumping.png",
+                                                        List(BROWN_FUR, HIGH_EARS, LONG_TEETH, SHORT_FUR))
 
-  val NORMAL_BROWN_HIGH_EARS_NORMAL_TEETH_THICK_FUR = "/bunnies/brown/high_ears/normal_teeth/thick_fur/normal.png"
-  val JUMPING_BROWN_HIGH_EARS_NORMAL_TEETH_THICK_FUR = "/bunnies/brown/high_ears/normal_teeth/thick_fur/jumping.png"
+  val BrownHighEarsLongTeethThickFur: BunnyImage= BunnyImage(
+                                                        "/bunnies/brown/high_ears/long_teeth/thick_fur/normal.png",
+                                                        "/bunnies/brown/high_ears/long_teeth/thick_fur/jumping.png",
+                                                        List(BROWN_FUR, HIGH_EARS, LONG_TEETH, LONG_FUR))
 
-  val NORMAL_BROWN_LONG_EARS_LONG_TEETH_NORMAL_FUR = "/bunnies/brown/long_ears/long_teeth/normal_fur/normal.png"
-  val JUMPING_BROWN_LONG_EARS_LONG_TEETH_NORMAL_FUR = "/bunnies/brown/long_ears/long_teeth/normal_fur/jumping.png"
+  val BrownHighEarsNormalTeethNormalFur: BunnyImage= BunnyImage(
+                                                        "/bunnies/brown/high_ears/normal_teeth/normal_fur/normal.png",
+                                                        "/bunnies/brown/high_ears/normal_teeth/normal_fur/jumping.png",
+                                                        List(BROWN_FUR, HIGH_EARS, SHORT_TEETH, SHORT_FUR))
 
-  val NORMAL_BROWN_LONG_EARS_LONG_TEETH_THICK_FUR = "/bunnies/brown/long_ears/long_teeth/thick_fur/normal.png"
-  val JUMPING_BROWN_LONG_EARS_LONG_TEETH_THICK_FUR = "/bunnies/brown/long_ears/long_teeth/thick_fur/jumping.png"
+  val BrownHighEarsNormalTeethThickFur: BunnyImage= BunnyImage(
+                                                        "/bunnies/brown/high_ears/normal_teeth/thick_fur/normal.png",
+                                                        "/bunnies/brown/high_ears/normal_teeth/thick_fur/jumping.png",
+                                                        List(BROWN_FUR, HIGH_EARS, SHORT_TEETH, LONG_FUR))
 
-  val NORMAL_BROWN_LONG_EARS_NORMAL_TEETH_NORMAL_FUR = "/bunnies/brown/long_ears/normal_teeth/normal_fur/normal.png"
-  val JUMPING_BROWN_LONG_EARS_NORMAL_TEETH_NORMAL_FUR = "/bunnies/brown/long_ears/normal_teeth/normal_fur/jumping.png"
+  val BrownLongEarsLongTeethNormalFur: BunnyImage= BunnyImage(
+                                                        "/bunnies/brown/long_ears/long_teeth/normal_fur/normal.png",
+                                                        "/bunnies/brown/long_ears/long_teeth/normal_fur/jumping.png",
+                                                        List(BROWN_FUR, LOW_EARS, LONG_TEETH, SHORT_FUR))
 
-  val NORMAL_BROWN_LONG_EARS_NORMAL_TEETH_THICK_FUR = "/bunnies/brown/long_ears/normal_teeth/thick_fur/normal.png"
-  val JUMPING_BROWN_LONG_EARS_NORMAL_TEETH_THICK_FUR = "/bunnies/brown/long_ears/normal_teeth/thick_fur/jumping.png"
+  val BrownLongEarsLongTeethThickFur: BunnyImage= BunnyImage(
+                                                        "/bunnies/brown/long_ears/long_teeth/thick_fur/normal.png",
+                                                        "/bunnies/brown/long_ears/long_teeth/thick_fur/jumping.png",
+                                                        List(BROWN_FUR, LOW_EARS, LONG_TEETH, LONG_FUR))
 
-  val NORMAL_WHITE_HIGH_EARS_LONG_TEETH_NORMAL_FUR = "/bunnies/white/high_ears/long_teeth/normal_fur/normal.png"
-  val JUMPING_WHITE_HIGH_EARS_LONG_TEETH_NORMAL_FUR = "/bunnies/white/high_ears/long_teeth/normal_fur/jumping.png"
+  val BrownLongEarsNormalTeethNormalFur: BunnyImage= BunnyImage(
+                                                        "/bunnies/brown/long_ears/normal_teeth/normal_fur/normal.png",
+                                                        "/bunnies/brown/long_ears/normal_teeth/normal_fur/jumping.png",
+                                                        List(BROWN_FUR, LOW_EARS, SHORT_TEETH, SHORT_FUR))
 
-  val NORMAL_WHITE_HIGH_EARS_LONG_TEETH_THICK_FUR = "/bunnies/white/high_ears/long_teeth/thick_fur/normal.png"
-  val JUMPING_WHITE_HIGH_EARS_LONG_TEETH_THICK_FUR = "/bunnies/white/high_ears/long_teeth/thick_fur/jumping.png"
+  val BrownLongEarsNormalTeethThickFur: BunnyImage= BunnyImage(
+                                                        "/bunnies/brown/long_ears/normal_teeth/thick_fur/normal.png",
+                                                        "/bunnies/brown/long_ears/normal_teeth/thick_fur/jumping.png",
+                                                        List(BROWN_FUR, LOW_EARS, SHORT_TEETH, LONG_FUR))
 
-  val NORMAL_WHITE_HIGH_EARS_NORMAL_TEETH_NORMAL_FUR = "/bunnies/white/high_ears/normal_teeth/normal_fur/normal.png"
-  val JUMPING_WHITE_HIGH_EARS_NORMAL_TEETH_NORMAL_FUR = "/bunnies/white/high_ears/normal_teeth/normal_fur/jumping.png"
+  val WhiteHighEarsLongTeethNormalFur: BunnyImage= BunnyImage(
+                                                        "/bunnies/white/high_ears/long_teeth/normal_fur/normal.png",
+                                                        "/bunnies/white/high_ears/long_teeth/normal_fur/jumping.png",
+                                                        List(WHITE_FUR, HIGH_EARS, LONG_TEETH, SHORT_FUR))
 
-  val NORMAL_WHITE_HIGH_EARS_NORMAL_TEETH_THICK_FUR = "/bunnies/white/high_ears/normal_teeth/thick_fur/normal.png"
-  val JUMPING_WHITE_HIGH_EARS_NORMAL_TEETH_THICK_FUR = "/bunnies/white/high_ears/normal_teeth/thick_fur/jumping.png"
+  val WhiteHighEarsLongTeethThickFur: BunnyImage= BunnyImage(
+                                                        "/bunnies/white/high_ears/long_teeth/thick_fur/normal.png",
+                                                        "/bunnies/white/high_ears/long_teeth/thick_fur/jumping.png",
+                                                        List(WHITE_FUR, HIGH_EARS, LONG_TEETH, LONG_FUR))
 
-  val NORMAL_WHITE_LONG_EARS_LONG_TEETH_NORMAL_FUR = "/bunnies/white/long_ears/long_teeth/normal_fur/normal.png"
-  val JUMPING_WHITE_LONG_EARS_LONG_TEETH_NORMAL_FUR = "/bunnies/white/long_ears/long_teeth/normal_fur/jumping.png"
+  val WhiteHighEarsNormalTeethNormalFur: BunnyImage= BunnyImage(
+                                                        "/bunnies/white/high_ears/normal_teeth/normal_fur/normal.png",
+                                                        "/bunnies/white/high_ears/normal_teeth/normal_fur/jumping.png",
+                                                        List(WHITE_FUR, HIGH_EARS, SHORT_TEETH, SHORT_FUR))
 
-  val NORMAL_WHITE_LONG_EARS_LONG_TEETH_THICK_FUR = "/bunnies/white/long_ears/long_teeth/thick_fur/normal.png"
-  val JUMPING_WHITE_LONG_EARS_LONG_TEETH_THICK_FUR = "/bunnies/white/long_ears/long_teeth/thick_fur/jumping.png"
+  val WhiteHighEarsNormalTeethThickFur: BunnyImage= BunnyImage(
+                                                        "/bunnies/white/high_ears/normal_teeth/thick_fur/normal.png",
+                                                        "/bunnies/white/high_ears/normal_teeth/thick_fur/jumping.png",
+                                                        List(WHITE_FUR, HIGH_EARS, SHORT_TEETH, LONG_FUR))
 
-  val NORMAL_WHITE_LONG_EARS_NORMAL_TEETH_NORMAL_FUR = "/bunnies/white/long_ears/normal_teeth/normal_fur/normal.png"
-  val JUMPING_WHITE_LONG_EARS_NORMAL_TEETH_NORMAL_FUR = "/bunnies/white/long_ears/normal_teeth/normal_fur/jumping.png"
+  val WhiteLongEarsLongTeethNormalFur: BunnyImage= BunnyImage(
+                                                        "/bunnies/white/long_ears/long_teeth/normal_fur/normal.png",
+                                                        "/bunnies/white/long_ears/long_teeth/normal_fur/jumping.png",
+                                                        List(WHITE_FUR, LOW_EARS, LONG_TEETH, SHORT_FUR))
 
-  val NORMAL_WHITE_LONG_EARS_NORMAL_TEETH_THICK_FUR = "/bunnies/white/long_ears/normal_teeth/thick_fur/normal.png"
-  val JUMPING_WHITE_LONG_EARS_NORMAL_TEETH_THICK_FUR = "/bunnies/white/long_ears/normal_teeth/thick_fur/jumping.png"
+  val WhiteLongEarsLongTeethThickFur: BunnyImage= BunnyImage(
+                                                        "/bunnies/white/long_ears/long_teeth/thick_fur/normal.png",
+                                                        "/bunnies/white/long_ears/long_teeth/thick_fur/jumping.png",
+                                                        List(WHITE_FUR, LOW_EARS, LONG_TEETH, LONG_FUR))
 
+  val WhiteLongEarsNormalTeethNormalFur: BunnyImage= BunnyImage(
+                                                        "/bunnies/white/long_ears/normal_teeth/normal_fur/normal.png",
+                                                        "/bunnies/white/long_ears/normal_teeth/normal_fur/jumping.png",
+                                                        List(WHITE_FUR, LOW_EARS, SHORT_TEETH, SHORT_FUR))
+
+  val WhiteLongEarsNormalTeethThickFur: BunnyImage= BunnyImage(
+                                                        "/bunnies/white/long_ears/normal_teeth/thick_fur/normal.png",
+                                                        "/bunnies/white/long_ears/normal_teeth/thick_fur/jumping.png",
+                                                        List(WHITE_FUR, LOW_EARS, SHORT_TEETH, LONG_FUR))
+}
+
+object ImageType extends Enumeration {
+  type ImageType = Value
+  val Jumping, Normal = Value
+}
+
+object Conversions {
+
+  case class BunnyWrapper(bunny: Bunny, imageType: ImageType)
+
+  object BunnyWrapper {
+    implicit def pairToBunnyWrapper(pair: (Bunny, ImageType)): BunnyWrapper = BunnyWrapper(pair._1, pair._2)
+  }
+
+}
+
+object BunnyImageUtils {
 
   /** Retrieve the image associated to a Bunny
    * @param bunny the bunny on which the image will be computed
    * @return      the scalafx.scene.image.Image of the bunny on normal state
    * */
-  implicit def getImageFromBunny(): ImageWrapper = ???
-
-  case class ImageWrapper(normalImage: Image, jumpingImage: Image)
+  def bunnyToImage(wrapper: BunnyWrapper): Image = {
+    val filterMap = wrapper.bunny.genotype.phenotype.visibleTraits - Genes.JUMP
+    val bunnyImage = BunnyImage.values.find(enum => enum.alleles.toSet == filterMap.values.toSet)
+    wrapper.imageType match {
+      case Jumping => new Image(bunnyImage.get.urlJumping)
+      case Normal => new Image(bunnyImage.get.urlNormal)
+    }
+  }
 }
 
