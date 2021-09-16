e73e16e14edbb5f456f949e4306d34ead4050b5f
Martina Baiardi <martina.baiardi4@studio.unibo.it>
2021-09-16 09:41:44 +0200

    Update Controller methods to enable climate change

--- src/main/scala/controller/Controller.scala
+++ src/main/scala/controller/Controller.scala
@@ -1,15 +1,20 @@
 package controller
 
-import engine.SimulationEngine
+import engine.{SimulationEngine, SimulationHistory}
 import engine.SimulationEngine.simulationLoop
 import engine.SimulationHistory.initialize
-import model.world.Generation.{Environment, Population}
+import model.world.{Climate, Environment, Summer, Winter}
+import model.world.Environment.Factors
 
 object Controller {
 
-  def startSimulation(): Unit = {
-    initialize("env")
-    simulationLoop().unsafeRunAsyncAndForget()
+  def startSimulation(climate: Climate, factors: Factors): Unit = {
+    SimulationHistory changeEnvironmentClimate climate
+    simulationLoop unsafeRunAsyncAndForget()
   }
+
+  def setSummerClimate(): Unit = SimulationHistory changeEnvironmentClimate Summer()
+
+  def setWinterClimate(): Unit = SimulationHistory changeEnvironmentClimate Winter()
 
 }
