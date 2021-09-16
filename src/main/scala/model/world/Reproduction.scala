01b70bc2b573e5facd3f3482700a2a092a3ca7f3
alessia.rocco <alessia.rocco@studio.unibo.it>
2021-09-16 12:38:08 +0200

    rename anotherGene in anotherGenes

--- src/main/scala/model/world/Reproduction.scala
+++ src/main/scala/model/world/Reproduction.scala
@@ -39,12 +39,12 @@ object Reproduction {
                                                   Gene(gk, grandpaMomAllele, grandmaDadAllele),
                                                   Gene(gk, grandmaMomAllele, grandpaDadAllele),
                                                   Gene(gk, grandpaMomAllele, grandpaDadAllele) )
-      val anotherGene = Random.shuffle(genesOfReproduction)
+      val anotherGenes = Random.shuffle(genesOfReproduction)
 
       mutations match {
-        case None => childrenGenotypes = (for (i <- 0 until CHILDREN_EACH_COUPLE) yield childrenGenotypes(i) + anotherGene(i)).toList
+        case None => childrenGenotypes = (for (i <- 0 until CHILDREN_EACH_COUPLE) yield childrenGenotypes(i) + anotherGenes(i)).toList
         case Some(ms) =>
-          childrenGenotypes = (for (i <- 0 until CHILDREN_EACH_COUPLE) yield childrenGenotypes(i) + anotherGene(i)).toList
+          childrenGenotypes = (for (i <- 0 until CHILDREN_EACH_COUPLE) yield childrenGenotypes(i) + anotherGenes(i)).toList
           ms filter(_.geneKind == gk) foreach { m =>
             childrenGenotypes = childrenGenotypes(CHILDREN_EACH_COUPLE - 1) + Gene(m.geneKind, JustMutatedAllele(gk.mutated), JustMutatedAllele(gk.mutated)) :: childrenGenotypes.take(CHILDREN_EACH_COUPLE - 1)
           }
