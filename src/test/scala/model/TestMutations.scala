f08044544cbe4b20dd070fa04cf70d0b7513ce87
alessia.rocco <alessia.rocco@studio.unibo.it>
2021-09-16 12:35:29 +0200

    test for model mutation

--- src/test/scala/model/TestMutations.scala
+++ src/test/scala/model/TestMutations.scala
@@ -1,21 +1,46 @@
 package model
-import engine.SimulationHistory.{getPopulationForNextGeneration, introduceMutation, startNextGeneration}
-import model.Bunny.generateRandomFirstBunny
+import engine.SimulationHistory.introduceMutation
+import model.Bunny.generateBaseFirstBunny
 import model.genome.Genes
 import model.mutation.Mutation
-import model.world.Reproduction.{generateAllChildren, generateChildren, nextGenerationBunnies}
+import model.world.Reproduction.{generateChildren, nextGenerationBunnies}
 import org.scalatest.{FlatSpec, Matchers}
 
 class TestMutations extends FlatSpec with Matchers {
-  val children: Seq[Bunny] = generateChildren(generateRandomFirstBunny, generateRandomFirstBunny)
+  val children: Seq[Bunny] = generateChildren(generateBaseFirstBunny, generateBaseFirstBunny)
+  val mutationFurColor: Mutation = Mutation(Genes.FUR_COLOR, isDominant = true)
+  val mutationFurLength: Mutation = Mutation(Genes.FUR_LENGTH, isDominant = false)
+  val mutationTeeth: Mutation = Mutation(Genes.TEETH, isDominant = false)
 
-  "When introducing a Mutation" should "compare in only half population" in {
-    introduceMutation(Mutation(Genes.FUR_COLOR, true))
-    val nextGeneration = nextGenerationBunnies(children, Some(List(Mutation(Genes.FUR_COLOR, true))))
-    var nBunnyWithMutation = 0
-    nextGeneration filter(_.genotype.genes(Genes.FUR_COLOR).kind == Genes.FUR_COLOR) foreach {
-      nBunnyWithMutation = nBunnyWithMutation + 1
-    }
-    assert(nBunnyWithMutation == (12/2 + 1))
+  "When introducing a Mutation" should "compare to the utmost only half population" in {
+    introduceMutation(mutationFurColor)
+
+    val nextGeneration = nextGenerationBunnies(children, Some(List(mutationFurColor)))
+    val bunnyWithMutation = nextGeneration filter (_.genotype.phenotype.visibleTraits(Genes.FUR_COLOR) == Genes.FUR_COLOR.mutated)
+
+    assert( nextGeneration.length == children.length / 2 * 4 + children.length )
+    assert( bunnyWithMutation.length <= (nextGeneration.length - children.length)/2 + 1 )
   }
+
+  "When introducing more than one Mutations" should "compare only a Mutation for Bunny" in {
+    val mutations = List(mutationFurColor, mutationFurLength, mutationTeeth)
+    try {
+      introduceMutation(mutations.head)
+      introduceMutation(mutations(1))
+      introduceMutation(mutations(2))
+    } catch {
+      case _: MultipleDominanceAssignmentException  => println("Allele corresponding to the Mutation set before. Ignoring this method")
+    }
+
+    val nextGeneration = nextGenerationBunnies(children, Some(mutations))
+    val bunnyWithMutation = nextGeneration filter (b =>
+      b.genotype.phenotype.visibleTraits(Genes.FUR_COLOR) == Genes.FUR_COLOR.mutated ||
+      b.genotype.phenotype.visibleTraits(Genes.FUR_COLOR) == Genes.FUR_COLOR.mutated ||
+      b.genotype.phenotype.visibleTraits(Genes.TEETH) == Genes.TEETH.mutated )
+
+    assert( nextGeneration.length == children.length / 2 * 4 + children.length )
+    assert( bunnyWithMutation.length <= (nextGeneration.length - children.length) / 2 + 1 )
+    assert( bunnyWithMutation.length <= (nextGeneration.length -  children.length))
+  }
+
 }
