9d9697993ea3337a05bce6eb31c49faf9e7688b3
aslucc <asia.lucchi@studio.unibo.it>
2021-09-22 14:31:53 +0200

    First version of style

--- src/main/scala/controller/Controller.scala
+++ src/main/scala/controller/Controller.scala
@@ -1,37 +1,45 @@
 package controller
 
-import engine.{SimulationEngine, SimulationHistory}
 import engine.SimulationEngine.simulationLoop
+import engine.SimulationHistory
 import model.mutation.Mutation
-import model.world.{Climate, Environment, Summer, Winter}
 import model.world.Environment.Factors
 import model.world.Generation.Population
+import model.world.{ Climate, Summer, Winter }
 import scalafx.application.Platform
 import view.scalaFX.ScalaFXView
 
-
 object Controller {
 
-  /** Method that starts the simulation
-   * @param climate the Environment Climate
-   * @param factors the Environment Factors*/
+  /**
+   * Method that starts the simulation
+   * @param climate
+   *   the Environment Climate
+   * @param factors
+   *   the Environment Factors
+   */
   def startSimulation(climate: Climate, factors: Factors): Unit = {
-    SimulationHistory changeEnvironmentClimate climate
+    SimulationHistory.changeEnvironmentClimate(climate)
     simulationLoop().unsafeRunAsyncAndForget()
   }
 
   /** Method that sets the Summer Climate inside Environment */
-  def setSummerClimate(): Unit = SimulationHistory changeEnvironmentClimate Summer()
+  def setSummerClimate(): Unit = SimulationHistory.changeEnvironmentClimate(Summer())
 
   /** Method that sets the Winter Climate inside Environment */
-  def setWinterClimate(): Unit = SimulationHistory changeEnvironmentClimate Winter()
+  def setWinterClimate(): Unit = SimulationHistory.changeEnvironmentClimate(Winter())
 
-  /** Method that insert a mutation inside the simulation
-   * @param mutation the Mutation */
-  def insertMutation(mutation: Mutation): Unit = SimulationHistory introduceMutation mutation
+  /**
+   * Method that insert a mutation inside the simulation
+   * @param mutation
+   *   the Mutation
+   */
+  def insertMutation(mutation: Mutation): Unit = SimulationHistory.introduceMutation(mutation)
 
   /** Method that shows the end of the simulation on the Application GUI */
-  def showEnd(): Unit = Platform runLater { ScalaFXView.showEnd() }
+  def showEnd(): Unit = Platform.runLater {
+    ScalaFXView.showEnd()
+  }
 
   def population: Population = SimulationHistory.getActualPopulation
 
