6d37eb03c28ba4c72c7ccbef8090959bcf4bef02
Marta Spadoni <marta.spadoni2@studio.unibo.it>
2021-10-02 10:40:26 +0200

    refactoring of introduce and remove factor methods in Enviroment and pimped List

--- src/main/scala/util/PimpScala.scala
+++ src/main/scala/util/PimpScala.scala
@@ -1,5 +1,9 @@
 package util
 
+import model.world.Environment.Factors
+import model.world.FactorsUtils.FactorTypes.FoodFactorKind
+import model.world.{ Factor, FoodFactor }
+
 object PimpScala {
 
   implicit class RichOption[A](option: Option[A]) {
@@ -9,6 +13,22 @@ object PimpScala {
 
   implicit class RichTuple2[A](tuple: (A, A)) {
     def toSeq: Seq[A] = Seq(tuple._1, tuple._2)
+  }
+
+  implicit class RichList[A](list: List[A]) {
+    def -?(pred: A => Boolean): List[A] = list.filterNot(pred)
+  }
+
+  implicit class RichFactorList(factors: Factors) {
+    def foodFactorIsPresent: Boolean = factors.exists(_.factorType == FoodFactorKind)
+    def getFoodFactor: FoodFactor = factors.filter(_.factorType == FoodFactorKind).head.asInstanceOf[FoodFactor]
+
+    def combineFoodFactor(newFoodFactor: FoodFactor): Factors =
+      factors.getFoodFactor.combineWith(newFoodFactor) :: factors -? (_.factorType == FoodFactorKind)
+
+    def updateFoodFactor(subFactor: FoodFactor): Factors =
+      (factors.getFoodFactor removeSubFactor (subFactor)) :: factors -? (_.factorType == FoodFactorKind)
+
   }
 
 }
