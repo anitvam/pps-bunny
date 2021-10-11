adea29ba93a8ad833049c32ef48a201578da438d
Martina <39519366+anitvam@users.noreply.github.com>
2021-10-11 21:53:03 +0200

    Update src/main/scala/it/unibo/pps/bunny/controller/Controller.scala
    Co-authored-by: aslucc <65169380+aslucc@users.noreply.github.com>

--- src/main/scala/it/unibo/pps/bunny/controller/Controller.scala
+++ src/main/scala/it/unibo/pps/bunny/controller/Controller.scala
@@ -39,7 +39,7 @@ object Controller {
   /**
    * Method that insert a recessive mutation inside the simulation for the given [[GeneKind]]
    * @param geneKind
-   *   the recessive gene of the mutation
+   *   the [[GeneKind]] for which a recessive mutation will be inserted
    */
   def insertRecessiveMutationFor(geneKind: GeneKind): Unit = insertMutation(recessiveMutation(geneKind))
 
